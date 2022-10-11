7ffafe358b5bf1ac35c57f622a7693f3287a0b7b
suraj Thomas <suraj.thomas@focussoftek.net>
2022-10-11 20:07:24 +0530

    first commit

---
+++ PhotoAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoAPP
+//
+//  Created by suraj thomas on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PhotoAPPTests/signUpFormModelValidator.swift
@@ -0,0 +1,22 @@
+//
+//  signUpFormModelValidator.swift
+//  PhotoAPPTests
+//
+//  Created by suraj thomas on 11/10/22.
+//
+
+import XCTest
+
+final class signUpFormModelValidator: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+
+}
