dfaefa41abe53d90bde0bc67681aefb65f7e67a6
suraj Thomas <ksurajt@gmail.com>
2022-11-23 12:48:03 +0530

    testSignupWebservice_WhenEmptyURLStringProvided_ReturnsError

--- PhotoAPPTests/Network/signupWebservicesTest.swift
+++ PhotoAPPTests/Network/signupWebservicesTest.swift
@@ -36,6 +36,7 @@ class SignupWebServiceTests: XCTestCase {
     func testSignupWebService_WhenGivenSuccessfullResponse_ReturnsSuccess() {
         
         // Arrange
+        
         let jsonString = "{\"status\":\"ok\"}"
         MockURLProtocol.stubResponseData =  jsonString.data(using: .utf8)
         
@@ -75,7 +76,22 @@ class SignupWebServiceTests: XCTestCase {
         self.wait(for: [expectation], timeout: 5)
     }
     
-    
+    func testSignupWebservice_WhenEmptyURLStringProvided_ReturnsError() {
+        //Arrange
+        let expectation = self.expectation(description: "An empty request URL string expectation")
+
+        sut = SignupWebService(urlString: "")
+        
+        //Act
+        sut.signup(withForm: signFormRequestModel) { (signupResponseModel, error) in
+            //Assert
+            XCTAssertNil(signupResponseModel)
+            XCTAssertEqual(error, SignupError.invalidRequestURLString)
+            expectation.fulfill()
+        }
+        
+        self.wait(for: [expectation], timeout: 2)
+    }
     
    
 }
